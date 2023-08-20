ff6970fb295a6bb4f5ef2f90d2dc8a11c97a1339
atomlayer <anfulu36484@gmail.com>
2023-08-20 18:27:15 +0300

    bug fixed

--- settings.py
+++ settings.py
@@ -1,10 +1,8 @@
 
 
-oobabooga_api_name = "Miku"
+oobabooga_api_name = "Lisa"
 
-# This is how you will address the voice assistant.
-# Sometimes the recognition may not work correctly, so you can add multiple address options.
-wake_words = ["miku", "miko", "mikko"]
+wake_words = ["lisa"]
 
 device = "cuda"
 
@@ -14,8 +12,9 @@ device = "cuda"
 whisper_model = "medium"  #"tiny", "base", "small", "medium", "large"
 non_english = False       #True - don't use the english model.
 energy_threshold = 1000   #Energy level for mic to detect.
-record_timeout = 2        #How real time the recording is in seconds.
-phrase_timeout = 3        #How much empty space between recordings before we consider it a new line in the transcription.
+record_timeout = 5        #How real time the recording is in seconds.
+phrase_timeout = 4        #How much empty space between recordings before we consider it a new line in the transcription.
+
 
 #endregion
 
